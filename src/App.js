523276548894cfe9c0a91048c36f27b680a26d47
Toderdene <72442586+Toderdenemn@users.noreply.github.com>
2023-01-03 21:12:48 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,62 +1,54 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import Card from "react-bootstrap/Card";
 
-function BasicExample() {
+import "react-awesome-button/dist/styles.css";
+import { AwesomeButton } from "react-awesome-button";
+
+function TextControlsExample() {
   return (
     <>
-      <Navbar bg="dark" expand="lg">
-        <Container>
-          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">
-                  Something
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  Separated link
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-
-      <Form style>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <div>
+        <Card style={{ width: "30rem", marginTop: "30px" }}>
+          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+          <Card.Body>
+            <Card.Title>Улс төр</Card.Title>
+            <Card.Text>
+              Н.ТАВИНБЭХ: НИЙСЛЭЛИЙН АГААРЫН БОХИРДОЛД ШАХМАЛ ТҮЛШ, ДЦС-УУДЫН
+              УТААНЫ ҮЗҮҮЛЭХ НӨЛӨӨЛӨЛ ӨМНӨХ ЖИЛҮҮДЭЭС ИХСЭЭГҮЙ
+            </Card.Text>
+            <AwesomeButton type="anchor">Anchor</AwesomeButton>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Card style={{ width: "30rem", marginTop: "30px" }}>
+          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+          <Card.Body>
+            <Card.Title>Нийгэм</Card.Title>
+            <Card.Text>
+              С.АМАРСАЙХАН: ЦАГ АГААРААС ШАЛТГААЛАН МАЛЫН ӨВС ТЭЖЭЭЛИЙН ХЭРЭГЛЭЭ
+              ӨНГӨРСӨН ЖИЛИЙНХЭЭС 2-4 ДАХИН ӨСЛӨӨ
+            </Card.Text>
+            <Button variant="primary">Дэлгэрэнгүй</Button>
+          </Card.Body>
+        </Card>
+      </div>
+      <div>
+        <Card style={{ width: "30rem", marginTop: "30px" }}>
+          {/* <Card.Img variant="top" src="holder.js/100px180" /> */}
+          <Card.Body>
+            <Card.Title>Технологи</Card.Title>
+            <Card.Text>
+              САМСУНГ “ХААЛГАН ДЭЭРЭЭ ЗУРАГТТАЙ” ХӨРГӨГЧ ТАНИЛЦУУЛЖЭЭ
+            </Card.Text>
+            <Button variant="primary">Дэлгэрэнгүй</Button>
+          </Card.Body>
+        </Card>
+      </div>
     </>
   );
 }
 
-export default BasicExample;
+export default TextControlsExample;
